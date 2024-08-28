d1e718839cc5749fe07230f08d7ff333672acedc
TevinH <hanleyt2@gmail.com>
2024-08-28 13:46:47 +0200

    added serializers and views for current and location

--- apps/weather/serializers/location.py
+++ apps/weather/serializers/location.py
@@ -5,4 +5,4 @@ from apps.weather.models import Location
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
-        fields = "__all__"
+        exclude = ('id',)
--- apps/weather/urls.py
+++ apps/weather/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'weather-urls'
 urlpatterns = [
         path('location/', views.location_list, name='location-list'),
-        path('current/', views.current_list, name='current-list'),
+        path('current/', views.CurrentWeatherView.as_view(), name='current-list'),
         path('forecast/', views.forecast_list, name='forecast-list'),
         path('alerts/', views.AlertsView.as_view(), name='alerts-view'),
         path('news/', views.NewsView.as_view(), name='news-view'),

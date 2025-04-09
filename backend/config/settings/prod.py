ea79f972f7e226d2b03a10c891d748168654f583
geekpnx <geekpnx@gmail.com>
2025-04-09 12:56:04 +0200

    - modified: backend/config/settings/prod.py - modified: backend/requirements/prod.txt

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 import environs
-
+import os
 env = environs.Env()
 
 env.read_env(str(BASE_DIR / '.env'))
@@ -28,3 +28,17 @@ DATABASES = {
 WEATHER_API_KEY = env('WEATHER_API_KEY')
 OPENWEATHERMAP_API_KEY = env('OPENWEATHERMAP_API_KEY')
 NEWS_API_KEY = env('NEWS_API_KEY')
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media files (user uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
+]
